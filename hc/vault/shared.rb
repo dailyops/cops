944e9b2a8bb2668b7ee67d8f0ee7f17b3b5420b0
Ruijian Cao <cao7113@hotmail.com>
2018-10-22 17:51:38 +0800

    fix

--- hc/vault/shared.rb
+++ hc/vault/shared.rb
@@ -12,12 +12,64 @@ custom_commands do
     Desc
   end
 
-  desc 'login', 'login as root'
-  def login
-    # hot to get authenticated/login status
+  desc 'login_root', 'login as root'
+  def login_root
     container_run <<~Desc
-      vault login #{conf_hash['root_token']}
+      vault login #{root_token}
+      sh
     Desc
+  end
+  # how to logout?? revoke the token???
+  
+  ##################################################
+  #               AUTH METHODS
+  desc 'auths', 'list auth methods'
+  def auths
+    container_run <<~Desc
+      vault login #{fetch(:root_token)}
+      vault auth list
+    Desc
+  end
+
+  # https://www.vaultproject.io/docs/auth/github.html
+  # most useful for humans: operators or developers using Vault directly via the CLI.
+  # friendly to operators and machines
+  desc 'github_config', ''
+  def github_config(user = 'cao7113', org = 'dailyops')
+    gtoken = github_access_token
+    container_run <<~Desc
+      vault login #{fetch(:root_token)}
+      # enable it
+      vault auth enable github
+      # configure with which org?
+      vault write auth/github/config organization=#{org}
+      # map users/teams to vault core policies
+      #vault write auth/github/map/teams/dev value=github-dev-policy
+      #todo keys share in teams
+      
+      # create mappings for a specific user
+      #vault write auth/github/map/users/#{user} value=github-test-policy
+      #GitHub user called #{user} will be assigned the policy + team policies.
+    Desc
+    #vault read auth/github/map/users/cao7113
+    #vault write -f auth/github/map/users/cao7113
+  end
+
+  desc 'github_login', ''
+  def github_login
+    # * using a GitHub personal access token
+    # * any valid GitHub access token with the read:org scope can be used for authentication
+    # * If such a token is stolen from a third party service, and the attacker is able to make network calls to Vault, they will be able to log in as the user that generated the access token. When using this method it is a good idea to ensure that access to Vault is restricted at a network level rather than public. 
+    gtoken = github_access_token
+    container_run <<~Desc
+      # will prompt by default if blank
+      vault login -method=github token="#{gtoken}"
+      sh
+    Desc
+    #curl \
+      #--request POST \
+      #--data '{"token": "MY_TOKEN"}' \
+      #http://127.0.0.1:8200/v1/auth/github/login
   end
 
   desc 'rclient', ''
@@ -45,6 +97,10 @@ custom_commands do
       else
         abort "require block!"
       end
+    end
+
+    def github_access_token
+      ENV['GITHUB_VAULT_TOKEN']
     end
   end
 end
